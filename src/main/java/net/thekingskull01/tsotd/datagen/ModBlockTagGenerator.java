ce21670b09958c6af839c7b36ada51f63e892262
THEKINGSKULL01 <92950949+THEKINGSKULL01@users.noreply.github.com>
2024-05-27 23:25:02 -0500

    Initial commit

--- src/main/java/net/thekingskull01/tsotd/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/thekingskull01/tsotd/datagen/ModBlockTagGenerator.java
@@ -2,6 +2,7 @@ package net.thekingskull01.tsotd.datagen;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
 import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.thekingskull01.tsotd.TSOTD;
@@ -24,6 +25,33 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
                 .add(ModBlocks.Dark_Crystal_Ore.get());
         this.tag(ModTags.Blocks.LIGHT_CRYSTAL_DETECTOR_VALUABLES)
                 .add(ModBlocks.Light_Crystal_Ore.get());
+
+        //Mineable with pickaxe
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.Dark_Crystal_Ore.get(),
+                        ModBlocks.Dark_Crystal_Block.get(),
+                        ModBlocks.Light_Crystal_Ore.get(),
+                        ModBlocks.Light_Crystal_Block.get(),
+                        ModBlocks.Coal_Crystal_Ore.get(),
+                        ModBlocks.Coal_Crystal_Block.get());
+
+        //Netherite tools
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
+                .add(ModBlocks.Dark_Crystal_Ore.get(),
+                        ModBlocks.Dark_Crystal_Block.get(),
+                        ModBlocks.Light_Crystal_Ore.get(),
+                        ModBlocks.Light_Crystal_Block.get(),
+                        ModBlocks.Coal_Crystal_Ore.get(),
+                        ModBlocks.Coal_Crystal_Block.get());
+
+
+        //mineable with axe
+
+        //mineable with shovel
+
+        //mineable with hoe
 
     }
 
