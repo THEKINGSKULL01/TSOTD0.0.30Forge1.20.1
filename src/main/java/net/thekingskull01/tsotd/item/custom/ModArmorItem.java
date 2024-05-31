5cf89d067249bcdf6b78f78e9e72e28a0f0433a6
THEKINGSKULL01 <92950949+THEKINGSKULL01@users.noreply.github.com>
2024-05-31 16:25:32 -0500

    Initial commit

--- src/main/java/net/thekingskull01/tsotd/item/custom/ModArmorItem.java
+++ src/main/java/net/thekingskull01/tsotd/item/custom/ModArmorItem.java
@@ -1,32 +1,35 @@
 package net.thekingskull01.tsotd.item.custom;
 
 import com.google.common.collect.ImmutableMap;
+import net.minecraft.world.entity.Entity;
+import net.thekingskull01.tsotd.item.ModArmorMaterials;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.thekingskull01.tsotd.item.ModArmorMaterials;
 
+import java.util.List;
 import java.util.Map;
 
 public class ModArmorItem extends ArmorItem {
-    private static final Map<ArmorMaterial, MobEffectInstance> MATERIAL_TO_EFFECT_MAP =
-            (new ImmutableMap.Builder<ArmorMaterial, MobEffectInstance>())
-            .put(ModArmorMaterials.TAKICHIRUM, new MobEffectInstance(MobEffects.ABSORPTION, 200, 2))
+    private static final Map<ArmorMaterial, List<MobEffectInstance>> MATERIAL_TO_EFFECT_MAP =
+            (new ImmutableMap.Builder<ArmorMaterial, List<MobEffectInstance>>())
+                    .put(ModArmorMaterials.TAKICHIRUM, List.of(
+                            new MobEffectInstance(MobEffects.ABSORPTION, 200, 1,
+                                    false,false, true),
+                            new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200, 2,
+                                    false,false, true),
+                            new MobEffectInstance(MobEffects.DIG_SPEED, 200, 2,
+                                    false,false, true),
+                            new MobEffectInstance(MobEffects.JUMP, 200, 2,
+                                    false,false, true))).build();
 
-
-
-                    .build();
-
-
-    public ModArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
-        super(pMaterial, pType, pProperties);
+    public ModArmorItem(ArmorMaterial material, Type slot, Properties settings) {
+        super(material, slot, settings);
     }
-
 
     @Override
     public void inventoryTick(ItemStack stack, Level level, Entity entity, int pSlotId, boolean pIsSelected) {
@@ -40,59 +43,48 @@ public class ModArmorItem extends ArmorItem {
     }
 
     private void evaluateArmorEffects(Player player) {
-
-        for (Map.Entry<ArmorMaterial, MobEffectInstance> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
+        for (Map.Entry<ArmorMaterial, List<MobEffectInstance>> entry : MATERIAL_TO_EFFECT_MAP.entrySet()) {
             ArmorMaterial mapArmorMaterial = entry.getKey();
-            MobEffectInstance mapEffect = entry.getValue();
+            List<MobEffectInstance> mapStatusEffects = entry.getValue();
 
-            if (hasPlayerCorrectArmorOn(mapArmorMaterial, player)) {
-
-                addEffectToPlayer(player, mapEffect);
-
+            if(hasCorrectArmorOn(mapArmorMaterial, player)) {
+                addStatusEffectForMaterial(player, mapArmorMaterial, mapStatusEffects);
             }
         }
-
     }
 
-    private void addEffectToPlayer(Player player, MobEffectInstance mapEffect) {
+    private void addStatusEffectForMaterial(Player player, ArmorMaterial mapArmorMaterial,
+                                            List<MobEffectInstance> mapStatusEffect) {
+        boolean hasPlayerEffect = mapStatusEffect.stream().anyMatch(effect -> player.hasEffect(effect.getEffect()));
 
-        boolean hasPlayerEffect = player.hasEffect(mapEffect.getEffect());
-
-        if (!hasPlayerEffect) {
-            player.addEffect(new MobEffectInstance(mapEffect.getEffect(),
-                    mapEffect.getDuration(), mapEffect.getAmplifier()));
+        if(hasCorrectArmorOn(mapArmorMaterial, player) && !hasPlayerEffect) {
+            mapStatusEffect.stream().forEach(effect -> player.addEffect(new MobEffectInstance(effect)));
         }
-
     }
 
-    private boolean hasPlayerCorrectArmorOn(ArmorMaterial mapArmorMaterial, Player player) {
+    private boolean hasFullSuitOfArmorOn(Player player) {
+        ItemStack boots = player.getInventory().getArmor(0);
+        ItemStack leggings = player.getInventory().getArmor(1);
+        ItemStack breastplate = player.getInventory().getArmor(2);
+        ItemStack helmet = player.getInventory().getArmor(3);
 
-        for (ItemStack armorStack : player.getArmorSlots()) {
-            if (!(armorStack.getItem() instanceof ArmorItem)) {
+        return !helmet.isEmpty() && !breastplate.isEmpty()
+                && !leggings.isEmpty() && !boots.isEmpty();
+    }
+
+    private boolean hasCorrectArmorOn(ArmorMaterial material, Player player) {
+        for (ItemStack armorStack : player.getInventory().armor) {
+            if(!(armorStack.getItem() instanceof ArmorItem)) {
                 return false;
             }
         }
 
-        ArmorItem helmet = ((ArmorItem) player.getInventory().getArmor(0).getItem());
-        ArmorItem chestplate = ((ArmorItem) player.getInventory().getArmor(1).getItem());
-        ArmorItem leggings = ((ArmorItem) player.getInventory().getArmor(2).getItem());
-        ArmorItem boots = ((ArmorItem) player.getInventory().getArmor(3).getItem());
+        ArmorItem boots = ((ArmorItem)player.getInventory().getArmor(0).getItem());
+        ArmorItem leggings = ((ArmorItem)player.getInventory().getArmor(1).getItem());
+        ArmorItem breastplate = ((ArmorItem)player.getInventory().getArmor(2).getItem());
+        ArmorItem helmet = ((ArmorItem)player.getInventory().getArmor(3).getItem());
 
-        return helmet.getMaterial() == mapArmorMaterial && chestplate.getMaterial() == mapArmorMaterial && leggings.getMaterial() == mapArmorMaterial && boots.getMaterial() == mapArmorMaterial;
-
+        return helmet.getMaterial() == material && breastplate.getMaterial() == material &&
+                leggings.getMaterial() == material && boots.getMaterial() == material;
     }
-
-    private boolean hasFullSuitOfArmorOn(Player player) {
-        ItemStack helmet = player.getInventory().getArmor(0);
-        ItemStack chestplate = player.getInventory().getArmor(1);
-        ItemStack leggings = player.getInventory().getArmor(2);
-        ItemStack boots = player.getInventory().getArmor(3);
-
-        return !helmet.isEmpty() && !chestplate.isEmpty() && !leggings.isEmpty() && !boots.isEmpty();
-
-
-
-    }
-
 }
-
